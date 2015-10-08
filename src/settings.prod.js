cb5f2f36737cd4e522f99a30dcc771e43d342649
robinComa <robin.coma.public@gmail.com>
2015-10-08 22:47:41 +0200

    amazon info

--- src/settings.prod.js
+++ src/settings.prod.js
@@ -7,14 +7,14 @@ const settings = {
         }
     },
     database: {
-        domain: 'http://localhost',
-        port: 5984,
+        domain: 'http://ec2-52-18-20-168.eu-west-1.compute.amazonaws.com',
+        port: 80,
         name: {
             user: 'app_prod_user',
             dialog: 'app_prod_dialog'
         },
         auth: {
-            username: 'secretlove',
+            username: 'secret-love',
             password: 'P@$$w0rd'
         }
     }
