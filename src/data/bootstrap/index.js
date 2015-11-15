c284b9cf8c76aac9a526a178c4c856e828a66bf1
Robin Coma Delperier <robin.coma.public@gmail.com>
2015-11-16 00:30:16 +0100

    bootstrap database ok : database creation + views creation

--- src/data/bootstrap/index.js
+++ src/data/bootstrap/index.js
@@ -1,24 +1,67 @@
 var Q = require('q');
+var fs = require('fs');
 var cradle = require('cradle');
-var views = require('./views.json');
+var dbSettings = require('./db-settings.json');
 
 var connection = new(cradle.Connection)(settings.database.domain, settings.database.port, {
     auth: settings.database.auth
 });
 
-var createView = function(name, map, reduce){
+var createViews = function(db, alias, views){
   var deferred = Q.defer();
-  setTimeout(function(){
-    console.log(name, map, reduce);
-    deferred.resolve();
-  }, 1000);
+  for(var name in views){
+    views[name].map = fs.readFileSync(views[name].map, 'utf8');
+    if(views[name].reduce){
+      views[name].reduce = fs.readFileSync(views[name].reduce, 'utf8');
+    }
+  }
+  db.save('_design/' + alias, views, function(err){
+    if(err){
+      deferred.reject();
+    }else{
+      for(var name in views){
+        console.info('View %s was created into %s database.', name, databaseAlias);
+      }
+      deferred.resolve();
+    }
+  });
   return deferred.promise;
 };
 
-var promises = [];
-for(var name in views){
-  var view = views[name];
-  promises.push(createView(name, view.map, view.reduce));
+var createDesign = function(db, views){
+  var designPromises = [];
+  for(var alias in views){
+    designPromises.push(createViews(db, alias, views[alias]));
+  }
+  return Q.all(designPromises);
+};
+
+var createDatabase = function(alias, config){
+  var databaseName = settings.database.name[alias];
+  var deferred = Q.defer();
+  var db = connection.database(databaseName);
+
+  db.exists(function (err, exists) {
+    if (err) {
+        console.error('Fail to connect the database %s" : \n', databaseName, err);
+        deferred.reject();
+    } else if (exists) {
+        console.info('Database %s already exist.', databaseName);
+        createDesign(db, config.views).then(deferred.resolve, deferred.reject);
+    } else {
+        console.error('Database %s does not exists.', databaseName);
+        db.create(function(){
+          console.error('Database %s was created.', databaseName);
+          createDesign(db, config.views).then(deferred.resolve, deferred.reject);
+        }, deferred.reject);
+    }
+  });
+  return deferred.promise;
+};
+
+var databasesPromises = [];
+for(var databaseAlias in dbSettings){
+  databasesPromises.push(createDatabase(databaseAlias, dbSettings[databaseAlias]));
 }
 
-module.exports = Q.all(promises);
+module.exports = Q.all(databasesPromises);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 global.settings = global.settings || require('./settings.json');
 
-var bootstrap = require('./data/bootstrap/index');
 
-bootstrap.then(function(){
+console.log('\n**************************************************');
+console.log('***********   Bootstrap job started  *************');
+console.log('**************************************************\n');
+
+require('./data/bootstrap/index').then(function(){
+
+  console.log('\n**************************************************');
+  console.log('*********   Bootstrap job terminated  ************');
+  console.log('**************************************************\n');
+
   var server = require('./api/server');
   var routes = require('./api/routes');
 
   routes.init(server);
+}, function(error){
+  console.error(error)
 });
